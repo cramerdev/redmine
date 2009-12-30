934e786251559687c6d57be796e5d53b0e3616f7
Nathan Lloyd Smith <nlloyds@gmail.com>
2009-12-30 14:31:05 -0600

    updated url link js for 0.9

--- public/javascripts/cramerdev.js
+++ public/javascripts/cramerdev.js
@@ -8,10 +8,10 @@ document.observe("dom:loaded", function () {
  * Change any custom field that ends in "URL:" to a link containing the
  *  value
  */
-$$(".issue table tr td[valign='top'] b").each(function (label) {
+$$("table.attributes tr th").each(function (label) {
     var el, t;
     if (label.innerHTML.match(/^.*URL\:$/) !== null) {
-        el = label.up().next().down();
+        el = label.next();
         t = el.innerHTML;
         el.update(new Element("a", { href: t }).update(t));
     }
