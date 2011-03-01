7f9ec0afa8bc84f4e96d646b5c33cfb88fa009cf
Nathan L Smith <nlloyds@gmail.com>
2011-03-01 13:19:34 -0600

    schedule & plugin update

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,11 @@
 every 24.hours do
   rake "redmine:backlogs:generate_chart_data"
 end
+
+every 15.minutes do
+  rake "redmine:email:receive_imap host=imap.gmail.com port=993 username=redmine@cramerdev.com password=zxasqw12cvdfer34 ssl=true project=newhires unknown_user=accept no_permission_check=1"
+end
+
+every 15.minutes do
+  rake "redmine:email:receive_imap host=imap.gmail.com port=993 username=collegedegrees-ticket@cramerdev.com password=iewahL1i ssl=true project=cd-schools unknown_user=accept no_permission_check=1"
+end
