813c0bd40cd70617c6a6e629989a8ae88a7e1155
Nathan L Smith <nlloyds@gmail.com>
2011-09-14 11:08:43 -0500

    read imap from configuration.yml.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
-every 24.hours do
-  rake "redmine:backlogs:generate_chart_data"
+# Load the imap_addresses from the config and set them all up to be checked
+# periodically
+(((YAML.load_file('config/configuration.yml') || {})['production'] || {})['imap_addresses'] || []).each do |a|
+  every 15.minutes do
+    rake "redmine:email:receive_imap " + a.map {|k, v| "#{k}=#{v}" }.join(' ')
+  end
 end
