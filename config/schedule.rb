331bacce11224b4da0fc154b71dd945624625b5b
Nathan L Smith <nlloyds@gmail.com>
2011-09-14 13:46:48 -0500

    --silent is redundant in rake task

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ env :PATH, ENV['PATH']
 # periodically
 (((YAML.load_file('config/configuration.yml') || {})['production'] || {})['imap_addresses'] || []).each do |a|
   every 5.minutes do
-    rake "redmine:email:receive_imap --silent " + a.map {|k, v| "#{k}=#{v}" }.join(' ')
+    rake "redmine:email:receive_imap " + a.map {|k, v| "#{k}=#{v}" }.join(' ')
   end
 end
