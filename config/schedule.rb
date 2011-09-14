97e9d461bbce5af15ed8bb9f89316cb8c894fefb
Nathan L Smith <nlloyds@gmail.com>
2011-09-14 12:20:02 -0500

    imap schedule updates

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,11 @@
+# When using REE in a place that's not in the default path, run this with
+# something like `whenever -w PATH=$PATH`
+env :PATH, ENV['PATH']
+
 # Load the imap_addresses from the config and set them all up to be checked
 # periodically
 (((YAML.load_file('config/configuration.yml') || {})['production'] || {})['imap_addresses'] || []).each do |a|
-  every 15.minutes do
-    rake "redmine:email:receive_imap " + a.map {|k, v| "#{k}=#{v}" }.join(' ')
+  every 5.minutes do
+    rake "redmine:email:receive_imap --silent " + a.map {|k, v| "#{k}=#{v}" }.join(' ')
   end
 end
