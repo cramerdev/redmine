8d939a1ee670ac370a04cb05ee736ea4ce63b51c
Nathan L Smith <nlloyds@gmail.com>
2012-01-11 17:28:49 -0600

    rm old redmine_contacts

--- vendor/plugins/redmine_contacts/app/models/contact_custom_field.rb
+++
@@ -1,7 +0,0 @@
-class ContactCustomField < CustomField    
-  unloadable
-  
-  def type_name
-    :label_contact_plural
-  end
-end
--- vendor/plugins/redmine_contacts/app/models/note_custom_field.rb
+++
@@ -1,7 +0,0 @@
-class NoteCustomField < CustomField    
-  unloadable
-  
-  def type_name
-    :label_note_plural
-  end
-end
